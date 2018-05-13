e2cebe917fcb35d52fe97831df93c85e3a25981c
jayadeepshitole <shitolejayadeep@gmail.com>
2018-05-13 14:28:31 +0530

    Rearranging codes folder

--- codes/generalFunctions.py
+++ codes/generalFunctions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import h5py
 from PIL import Image
 
@@ -56,3 +57,19 @@ def mnistAsImage(x, file = "..\\images\\number.png"):
     x = np.reshape((28, 28)) 
     im = Image.fromarray(x).convert('L')
     im.save(file)
+    
+# Helper function to plot a decision boundary. 
+def plot_decision_boundary(pred_func, X, y): 
+    # Set min and max values and give it some padding 
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5 
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5 
+    h = 0.01 
+    # Generate a grid of points with distance h between them 
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h)) 
+    # Predict the function value for the whole gid 
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()].T) 
+    Z = Z.reshape(xx.shape)
+    Z = (Z >= 0.50) + 0
+    # Plot the contour and training examples 
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral) 
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral) 
--- codes/network.py
+++ codes/network.py
@@ -6,6 +6,7 @@ Created on Tue Apr 17 10:26:30 2018
 
 A module to implement stochastic gradient descent algorithm for feedforward neural network. 
 Gradients are calculated using backpropagation. 
+Reference: http://neuralnetworksanddeeplearning.com/chap1.html
 """
 
 import os
@@ -15,55 +16,133 @@ from mnist_loader import load_data_wrapper
 from generalFunctions import sigmoid, sigmoid_deri
 
 class Network(object):
-    """The list sizes contains the number of neurons in the respecitve layers of the netdwork. The
-    biases and weights are initialised randomly using gaussian distribution with mean 0 and variance 1.
-    Note that we assume the first layer as input layer without any biases.
-    """
+
     def __init__(self, sizes):
+        """
+        Argument:
+        sizes -- list of number of units in each layer of the neural network
+        
+        Return:
+        num_layers -- total number of layer in the neural network
+        sizes -- same as the argument
+        weights -- numpy.ndarray of weights for each layer 
+        bias -- numpy.ndarray of biases for each layer
+        """
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-#net = Network([2, 3, 1])
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        
+    def calculate_gradients(self, X, Y):
+        """
+        This function calculates the cost function and the gradient of the cost function with 
+        respect to weights and biases
+        
+        Argument:
+        X -- data of shape (number of features, number of examples)
+        Y -- true "label" of shape (number of categories, number of examples)
+        
+        Return:
+        grads -- a dictionary of dw, db where dw is the gradient of cost function wrt weights
+        of shape same as w and db is the gradient of cost function wrt biases of shape same as b
+        """
+        Z1 = np.matmul(self.weights[0], X) + self.biases[0]       #(30, m)
+        A1 = sigmoid(Z1)                                          #(30, m)
+        Z2 = np.matmul(self.weights[1], A1) + self.biases[1]       #(10, m)
+        A2 = sigmoid(Z2)                                          #(10, m)
+        # number of examples
+        m = X.shape[1]
+        dZ2 = A2 - Y                                              #(784, m)
+        dW2 = (1 / m) * np.matmul(dZ2, A1.T)                      #(10, 30)
+        db2 = (1 / m) * np.sum(dZ2, axis = 1, keepdims = True)    #(10, 1)
+        dZ1 = np.multiply(np.matmul(self.weights[1].T, dZ2), sigmoid_deri(Z1)) #(30, m)
+        dW1 = (1 / m) * np.matmul(dZ1, X.T)                       #(30, 784)
+        db1 = (1 / m) * np.sum(dZ1, axis = 1, keepdims = True)    #(30, 1)
+        
+        grads = {"dW1":dW1, "db1":db1, "dW2":dW2, "db2":db2}        
+        return grads
+        
+    def train(self, X_train, Y_train, X_test, Y_test, epochs = 100, batch_size = 32, learning_rate = 0.005):
+		"""
+		Updates weights and biases of neural network using batch gradient descent. This function prints loss function value and test accuracy after every 10 epochs.
+		
+		Arguments:
+		X_train -- training data, a numpy.ndarray of shape (num_px*num*px, m_train)
+		Y_train -- training data labels, a numpy.ndarray of shape (total number of categories, m_train)
+		X_test -- test data, a numpy.ndarray of shape (num_px*num*px, m_test)
+		Y_test -- test data labels, a numpy.ndarray of shape (total number of categories, m_test)
+		epochs -- number of learning iterations over the whole training data set
+		batch_size	-- number of training examples used in batch gradient descent to update weights
+		learning_rate -- learning rate used in updating the weights and biases
+		"""
+        m_train = X_train.shape[1]
+        for epoch in range(epochs + 1):
+            batch = np.arange(0, m_train)
+            np.random.shuffle(batch)
+            for k in range(m_train // batch_size + 1):
+                if k * batch_size <  m_train:
+                    X_mini_batch = X_train[:,batch[k * batch_size:(k + 1) * batch_size]]
+                    Y_mini_batch = Y_train[:,batch[k * batch_size:(k + 1) * batch_size]]
+                    self.update_weights(X_mini_batch, Y_mini_batch, learning_rate)
+            
+            if epoch % 10 == 0:          
+                # Loss function
+                A2 = self.feedforward(X_train)
+                cost = (1 / m_train) * np.sum(-np.multiply(Y_train, np.log(A2)) - np.multiply(1 - Y_train, np.log(1 - A2)))
+                
+                # Accutacy on training data
+                A2_test = self.feedforward(X_test)
+                class_pred = A2_test.argmax(axis = 0)
+                class_actual = Y_test.argmax(axis = 0)
+                acc = sum(class_actual == class_pred)
+                print(f"epoch:{epoch}, Cost: {cost}, accuracy:{acc}/{X_test.shape[1]}")
+                
+    def update_weights(self, X, Y, learning_rate):
+		"""
+		This function calculates gradients and updates weights
+		
+		Arguments:
+		X -- training data, mini batch of size (num_px*num*px, batch_size)
+		Y -- training labels, mini batch of size (number of categories, batch_size)
+		learning_rate -- learning rate 
+		"""
+        grads = self.calculate_gradients(X, Y)
+        #update weights and biases
+        self.weights[0] = self.weights[0] - learning_rate * grads["dW1"]
+        self.weights[1] = self.weights[1] - learning_rate * grads["dW2"]
+        self.biases[0] = self.biases[0] - learning_rate * grads["db1"]
+        self.biases[1] = self.biases[1] - learning_rate * grads["db2"]
+        
     
-    def feedforward(self, a):
-        """Return an numpy.ndarray of shape (num_layers - 1, 2), the value at [i, 0] denotes the z values 
-        and [i, 1] denotes the activation a for hidden layer i.
-        Where, z = wa + b and a = sigmoid(z).
-        For network of size 3, function will return [[z1, a1], [z2, a2]]
-        The input a is the training data - a numpy.ndarray of shape (n, 1) or (n, m)"""
-        za = []
-        for w, b in zip(self.weights, self.biases):
-            z = np.matmul(w, a) + b
-            a = sigmoid(z)
-            za.append([z,a])
-        return(np.array(za))
+    def feedforward(self, X):
+		"""
+		This function calculates the Y_pred, feedforward propagation.
+		
+		Argument:
+		X -- training data, a numpy.ndarray of shape (num_px*num*px, total number of examples)
+		
+		Return:
+		A2 -- feedforward output, Y_pred of size (number of categories, total number of examples)
+		"""
+        Z1 = np.matmul(self.weights[0], X) + self.biases[0]       #(30, m)
+        A1 = sigmoid(Z1)                                          #(30, m)
+        Z2 = np.matmul(self.weights[1], A1) + self.biases[1]       #(10, m)
+        A2 = sigmoid(Z2)                                          #(10, m)
+        return A2        
+        
     
-    def gradientDescent(self, training_data, learning_rate = 0.01):
-        
-        training_inputs, training_labels = zip(*training_data)
-        training_inputs = np.array(training_inputs)
-        training_inputs = training_inputs.reshape((training_inputs.shape[0], training_inputs.shape[1])).T
-        training_labels = np.array(training_labels)
-        training_labels = training_labels.reshape((training_labels.shape[0], training_labels.shape[1])).T
-        
-        grads_w = [np.zeros(w.shape) for w in self.weights]
-        grads_b = [np.zeros(b.shape) for b in self.biases]
-        
-        za = self.feedforward(training_inputs)
-        z = za[-1, 0]
-        a = za[-1, 1]
-        dz = a - training_labels
-        dw = da * np.dot(sigmoid_deri(z).shape)
-        
-        z2 = za[1, 0]
-        a2 = za[1, 1]
-        da2 = da3 * self.weights[2] * sigmoid_deri(z2)
-        
-        #a2 = za[1, 1]
-        #z2 = za[1, 0]
-        #da2 = -1* np.mean(training_data[1] - a2)
-
-training_data, validation_data, test_data = load_data_wrapper()
-net = Network([784, 10, 10, 1])
-za = net.GD(training_data = training_data)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
